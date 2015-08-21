3cfb17cc469fb12d08853391a30f7659fd13587c
WooKyoung Noh <wookay.noh@gmail.com>
2015-08-21 17:22:38 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# test scala
+Linux, OSX: [![Build Status](https://api.travis-ci.org/wookay/scalacat.svg?branch=master)](https://travis-ci.org/wookay/scalacat)
---
+++ TestCases/test_types.scala
@@ -0,0 +1,12 @@
+import org.scalatest.FunSuite
+ 
+class TestTypes extends FunSuite {
+ 
+  test("int") {
+    assert(3 == 1+2)
+  }
+ 
+  test("float") {
+    assert(3.14 == 3.14)
+  }
+}
